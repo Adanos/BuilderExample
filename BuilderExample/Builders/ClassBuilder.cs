d5e8e8a7b8c9dc20b820ebf30a82d9ab19341ee1
Adanos <polon1@op.pl>
2016-03-02 14:14:03 +0100

    Task #5

---
+++ BuilderExample/Builders/ClassBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace BuilderExample.Builders
+{
+	sealed class ClassBuilder
+	{
+		public string name { get; private set; }
+		public ISet<Field> fields { get; private set; }
+		public ISet<Property> properties { get; private set; }
+		public ISet<Method> methods { get; private set; }
+
+		public ClassBuilder(string name)
+		{
+			this.name = name;
+			fields = new HashSet<Field>();
+			properties = new HashSet<Property>();
+			methods = new HashSet<Method>();
+		}
+
+		public ClassBuilder addField(Field field)
+		{
+			fields.Add(field);
+			return this;
+		}
+
+		public ClassBuilder addProperty(Property property)
+		{
+			properties.Add(property);
+			return this;
+		}
+
+		public ClassBuilder addMethod(Method method)
+		{
+			methods.Add(method);
+			return this;
+		}
+
+		public ClassBuilder removeField(Field field)
+		{
+			fields.Remove(field);
+			return this;
+		}
+
+		public ClassBuilder removeProperty(Property property)
+		{
+			properties.Remove(property);
+			return this;
+		}
+
+		public ClassBuilder removeMethod(Method method)
+		{
+			methods.Remove(method);
+			return this;
+		}
+	}
+}
---
+++ BuilderExample/Class.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BuilderExample
+{
+	class Class
+	{
+		private readonly string name;
+		private readonly ISet<Field> fields;
+		private readonly ISet<Property> properties;
+		private readonly ISet<Method> methods;
+
+		public Class(Builders.ClassBuilder builder)
+		{
+			name = builder.name;
+			fields = builder.fields;
+			properties = builder.properties;
+			methods = builder.methods;
+		}
+	}
+}
