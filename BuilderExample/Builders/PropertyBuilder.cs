787ce0e3b22f995a7fb1d38e1317f1e7f81e5722
Adanos <polon1@op.pl>
2016-03-02 13:19:40 +0100

    Task #2

---
+++ BuilderExample/Builders/PropertyBuilder.cs
@@ -0,0 +1,29 @@
+﻿
+namespace BuilderExample.Builders
+{
+	sealed class PropertyBuilder
+	{
+		public string name { get; private set; }
+		public string value { get; private set; }
+		public string type { get; private set; }
+		public AccessModifier accessModifier { get; private set; }
+
+		public PropertyBuilder(string name, string type, AccessModifier accessModifier)
+		{
+			this.name = name;
+			this.type = type;
+			this.accessModifier = accessModifier;
+		}
+
+		public PropertyBuilder setValue(string value)
+		{
+			this.value = value;
+			return this;
+		}
+
+		public Property build()
+		{
+			return new Property(this);
+		}
+	}
+}
---
+++ BuilderExample/Property.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BuilderExample
+{
+	class Property
+	{
+		private readonly string name;
+		private readonly string value;
+		private readonly string type;
+		private readonly AccessModifier accessModifier;
+
+		public Property(Builders.PropertyBuilder builder)
+		{
+			this.name = builder.name;
+			this.type = builder.type;
+			this.accessModifier = builder.accessModifier;
+			this.value = builder.value;
+		}
+	}
+}
