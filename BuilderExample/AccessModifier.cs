34bc477a8963054104806eeed04470f89a4f2955
Adanos <polon1@op.pl>
2016-03-02 11:23:20 +0100

    Task #3

---
+++ BuilderExample/AccessModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderExample
+{
+	enum AccessModifier
+	{
+		Internal,
+		Private,
+		Protected,
+		Public
+	}
+}
