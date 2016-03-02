7ac1fa526687100d549fc16f491e8bf93595d986
Adanos <polon1@op.pl>
2016-03-02 11:11:59 +0100

    Task #1

---
+++ BuilderExample/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderExample
+{
+	class Field
+	{
+		private string key;
+		private string value;
+
+		public Field(string key, string value)
+		{
+			this.key = key;
+			this.value = value;
+		}
+	}
+}
